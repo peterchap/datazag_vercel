15e47fbbbdf4cd1da0a9c02cd594bf49110a5b96
Peter Chaplin <peterchaplin@btinternet.com>
2025-10-02 17:47:37 +0100

    fixed bug

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -7,7 +7,7 @@ import Stripe from 'stripe';
 import { Resend } from 'resend';
 import { redisSyncService } from '@/lib/redis-sync-client';
 
-export const runtime = 'enodejs'; // Ensure the route is treated as a Node.js function
+export const runtime = 'nodejs'; // Ensure the route is treated as a Node.js function
 export const dynamic = 'force-dynamic';
 
 const stripe = process.env.STRIPE_SECRET_KEY ? new Stripe(process.env.STRIPE_SECRET_KEY) : null;
